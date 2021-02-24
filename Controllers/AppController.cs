7d3c61f183bb013e276fcd6be8ebaa489768fab5
Warren Eliason <warren@convexum.co.za>
2021-02-24 16:57:52 +0200

    installed TS package

--- Controllers/AppController.cs
+++ Controllers/AppController.cs
@@ -5,8 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Convergence.Services;
-using Convergence.ViewModels;
+
 using Convergence.Data;
 
 
