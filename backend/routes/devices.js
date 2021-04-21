cc77bf36e4c5556f99d11ef0394fe2549bfba4a6
Warren Eliason <warren@convexum.co.za>
2021-04-21 15:54:17 +0200

    RequestQuerying

--- backend/routes/devices.js
+++ backend/routes/devices.js
@@ -5,6 +5,7 @@ var router = express.Router();
 var parseString = require('xml2js').parseString;
 
 
+
 request('https://wwweurope1.systemmonitor.eu.com/api/?apikey=6edf84904cef55f30a8df7dff0bcafdc&service=list_devices_at_client&clientid=339119&devicetype=workstation', function (error, response, body) {
   console.error('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
@@ -12,11 +13,10 @@ request('https://wwweurope1.systemmonitor.eu.com/api/?apikey=6edf84904cef55f30a8
 
   parseString(body, function (err, result) {
     console.dir(result);
+    console.log(body);
 });
 
 });
-
-
 
 
 
